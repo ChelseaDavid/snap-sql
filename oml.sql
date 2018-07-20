ebe5f6fe8a5fbf734c89211ccc2796b57789af70
Chelsea David <cryan17@cnm.edu>
2018-07-20 13:25:23 -0600

    dropped comment(s) table

--- oml.sql
+++ oml.sql
@@ -1,2 +1,3 @@
 INSERT INTO comment (commentId, commentText, commentDateTime) VALUES (UNHEX("62ba310098b140bbb55d318b70a34229") , "Matt is always to blame", "2018-07-21 08:34:17.023" );
 INSERT INTO comment (commentId, commentText, commentDateTime) VALUES (UNHEX("968218904c2747d197f7cd4ae03c745c") , " Matt causes several headaches","2018-07-22 10:35:24.321");
+DROP TABLE comments;
